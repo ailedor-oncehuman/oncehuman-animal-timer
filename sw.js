d5d6e22fa136ab817759af9a49f8906f30efffb3
ailedor <aquaflare1981@gmail.com>
2026-06-06 10:31:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE='oncehuman-animal-timer-ver4-v1';
-const ASSETS=['./','index.html','manifest.webmanifest','icon.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./'))))});
+const CACHE='oncehuman-timer-ver7-v1';
+const ASSETS=[
+ './','index.html','manifest.webmanifest','icon.png',
+ 'wolf.png','leopard.png','bear.png','boar.png','buffalo.png','polarbear.png',
+ 'rabbit.png','deer.png','sheep.png','capybara.png','fox.png','snowfox.png',
+ 'flamingo.png','crocodile.png','seaturtle.png','alligator_turtle.png'
+];
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./'))));
+});
